cae4c8b0155ed0a9c7d2cda093e7f3fed4d4e7b8
Yash Sharma <yash2003.ys@gmail.com>
2024-01-07 15:39:10 +0530

    changed favicon

--- frontend/PinIt/src/components/Sidebar.jsx
+++ frontend/PinIt/src/components/Sidebar.jsx
@@ -4,7 +4,6 @@ import {RiHomeFill} from 'react-icons/ri';
 import {IoIosArrowForward} from 'react-icons/io';
 import {categories} from '../utils/data';
 import pinItlogo from '../assets/pinItlogo.svg';
-import logo from "../assets/logo.png";
 
 const isNotActiveStyle='flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
 const isActiveStyle='flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize';
